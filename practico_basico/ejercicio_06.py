36e77496a4373ac309b4d9f9ec4162175038f3e7
pablobots <pabloxor@hotmail.com>
2021-04-01 20:46:35 -0300

    Agregar el ejer. 7 del practico basico Close #10

--- practico_basico/ejercicio_06.py
+++ practico_basico/ejercicio_06.py
@@ -191,31 +191,31 @@ Verificar si las siguientes listas son iguales
 Restricción: Utilizar el operador ==
 """
 
-lista_01 = [1.0,2,3,4]
-lista_02 = [1,2.00,3,4.0]
+lista_01 = [1,2,3,4.5,6,7]
+lista_02 = [1,3,2,4,5,6,7]
 
 # COMPLETAR - INICIO
 son_iguales = lista_01 == lista_02
 # COMPLETAR - FIN
 
-assert son_iguales
+assert not son_iguales
 
 
 """
 Se cuenta con una lista de elementos booleanos que corresponden a las notas de los exámenes 
 cuatrimestrales de un alumno (True si está aprobado y False en caso contrario)
-Determinar si el alumno no tiene al menos un exámen aprobado.
+Determinar si el alumno no tiene examenes aprobados.
 
 Restricción: Utilizar el método any
 """
 
-notas = [False, False, False, False, False, False, False, True, False, False, True, True]
+notas = [False, False, False, False, False, False, False, False, False]
 
 # COMPLETAR - INICIO
-no_tiene_al_menos_un_aprobado = not any(notas)
+no_tiene_examenes_aprobados= not any(notas)
 # COMPLETAR - FIN
 
-assert not no_tiene_al_menos_un_aprobado
+assert no_tiene_examenes_aprobados
 
 
 """
